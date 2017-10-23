0d7cd64342b01cce3fe8ff98b7ad7153ec54fc82
kobi <kobi.b@inner-active.com>
2017-10-24 01:13:00 +0300

    #12: Toolbar fixes

--- vnode/src/main/java/vm/component/IActivity.kt
+++ vnode/src/main/java/vm/component/IActivity.kt
@@ -1,7 +1,10 @@
 package vm.component
 
+import iview.IPopupMenu
+
 
 interface IActivity {
     fun openFragment(fragment: Any, addToBackStack: String)
     fun onBackPressed()
+    fun popupMenu(anchor: Any): IPopupMenu
 }
