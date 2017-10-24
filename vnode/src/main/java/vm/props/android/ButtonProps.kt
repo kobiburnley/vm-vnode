a850b3485b7cbd504a61bfa996321b93d765c696
kobi <kobi.b@inner-active.com>
2017-10-24 11:01:31 +0300

    Empty menu

--- vnode/src/main/java/vm/props/android/ButtonProps.kt
+++ vnode/src/main/java/vm/props/android/ButtonProps.kt
@@ -2,10 +2,12 @@ package vm.props.android
 
 interface IButtonProps : ITextViewProps {
     val title: String
+    val bg: Int
 }
 
 class ButtonProps(init: ButtonProps.() -> Unit) : IButtonProps, TextViewProps() {
     override lateinit var title: String
+    override var bg: Int = -1
 
     init {
         init()
